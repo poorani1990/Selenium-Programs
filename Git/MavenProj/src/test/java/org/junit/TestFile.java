f273e17b25ca7730d86c2bb28353b75750243644
poorani1990 <pooraninayai.k@gmail.com>
2022-11-10 17:46:19 +0530

    DineshGit

--- Git/MavenProj/src/test/java/org/junit/TestFile.java
+++ Git/MavenProj/src/test/java/org/junit/TestFile.java
@@ -1,6 +1,9 @@
 package org.junit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ById;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -11,10 +14,12 @@ public class TestFile {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://en-gb.facebook.com/");
-//Poorani	//XSSFWorkbook work = new XSSFWorkbook();
+	//XSSFWorkbook work = new XSSFWorkbook();
 		
 		
 		driver.manage().window().maximize();
+		WebElement username = driver.findElement(By.id("email"));
+		username.sendKeys("Poorani");
 		//XSSFWorkbook work = new XSSFWorkbook();
 	}
 }
