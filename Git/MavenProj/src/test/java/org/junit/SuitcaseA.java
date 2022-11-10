d2951ab5c4cc9923f40121bd839e05b64a9b0cc1
poorani1990 <pooraninayai.k@gmail.com>
2022-11-10 20:37:51 +0530

    TestFile

---
+++ Git/MavenProj/src/test/java/org/junit/SuitcaseA.java
@@ -0,0 +1,26 @@
+package org.junit;
+
+import org.junit.Test;
+
+public class SuitcaseA {
+	@Test
+	public void Test1() {
+		System.out.println("ClassA Test1");
+	}
+
+	@Test
+	public void Test2() {
+		System.out.println("ClassA Test2");
+	}
+
+	@Test
+	public void Test3() {
+		System.out.println("ClassA Test3");
+	}
+
+	@Test
+	public void Test4() {
+		System.out.println("ClassA Test4");
+	}
+
+	}
---
+++ Git/MavenProj/src/test/java/org/junit/TestFile.java
@@ -0,0 +1,17 @@
+package org.junit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestFile {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://en-gb.facebook.com/");
+		driver.manage().window().maximize();
+		//XSSFWorkbook work = new XSSFWorkbook();
+	}
+}
